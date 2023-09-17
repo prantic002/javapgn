9e72fec7cb97ae9f387e0d6d1f15f277d054cfd8
prantic002 <prantic1212@gmail.com>
2023-09-17 16:19:41 +0800

    ex3 done

---
+++ day6/ex3.js
@@ -0,0 +1,29 @@
+// Q1, Q2
+// const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya'
+//   ];
+// const copiedCountries = [...countries];
+// const copiedCountries = countries.slice();
+// let sh = copiedCountries.sort();
+// console.log(sh);
+// const webTechs = [
+//     'HTML',
+//     'CSS',
+//     'JavaScript',
+//     'React',
+//     'Redux',
+//     'Node',
+//     'MongoDB'
+//   ]
+//   const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+//   console.log(webTechs.sort(), mernStack.sort())
